e5a94fc3e2b2440d29b44209846c11c3b4843e77
AmbachtIT <34813625+AmbachtIT@users.noreply.github.com>
2023-06-26 20:45:21 +0200

    Some model validation

---
+++ WarmtePompGeluid.Test/InputGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using WarmtePompGeluid.Model;
+
+namespace WarmtePompGeluid.Test
+{
+    public class InputGenerator
+    {
+
+        public IEnumerable<Input> Generate() =>
+                Apply(Generate_AP(), Situatie.AP);
+
+        private IEnumerable<Input> Apply(IEnumerable<Input> inputs, Situatie situatie)
+        {
+            foreach (var input in inputs)
+            {
+                input.Situatie = situatie.Name;
+                yield return input;
+            }
+        }
+
+        private IEnumerable<Input> Generate_AP()
+        {
+            yield return Base_AP() with
+            {
+                PlanGegevens = new PlanGegevens()
+                {
+                    Omschrijving = "ok",
+                }
+            };
+        }
+
+        private Input Base_AP()
+        {
+            return new Input()
+            {
+                BronPositie = new Vector3(0, 0, 5),
+                OntvangstPosities = new List<OntvangstPositie>()
+                {
+                    new OntvangstPositie()
+                    {
+                        Positie = new Vector3(-3, 0, 5), // 5m naar links
+                        QGeluidsBron = 2,
+                        QOntvanger = 2,
+                        IsBuitenUnitAfgeschermd = false,
+                        IsRaamDeurMetBalkon = true
+                    }
+                }
+            };
+        }
+
+    }
+}
--- WarmtePompGeluid.Test/ModelValidation.cs
+++ WarmtePompGeluid.Test/ModelValidation.cs
@@ -6,6 +6,7 @@ using System.Net.WebSockets;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Ambacht.Common;
 using Ambacht.Common.Excel;
 using Ambacht.Common.Serialization;
 using WarmtePompGeluid.Model;
@@ -16,15 +17,9 @@ namespace WarmtePompGeluid.Test
     {
 
         [Test(), TestCaseSource(nameof(AllScenarios)), Explicit()]
-        public async Task Serialize((int, Situatie, Input) args)
+        public async Task Serialize(Input input)
         {
-            var (i, situatie, input) = args;
-            var dir = Path.Combine(SourceRoot, "data", "validation", situatie.Name);
-            if (!Directory.Exists(dir))
-            {
-                Directory.CreateDirectory(dir);
-            }
-            var outputPath = Path.Combine(dir, $"input-{i}.json");
+            var outputPath = GetOutputPath(input, "input");
 
             var json = _serializer.SerializeObject(input);
             await File.WriteAllTextAsync(outputPath, json);
@@ -32,42 +27,37 @@ namespace WarmtePompGeluid.Test
 
 
         [Test(), TestCaseSource(nameof(AllScenarios)), Explicit()]
-        public async Task RunExcel((int, Situatie, Input) args)
+        public async Task RunExcel(Input input)
         {
-            var (i, situatie, input) = args;
             var excelPath = Path.Combine(SourceRoot, "data", @"WPAC-geluid_V2020_0.xlsx");
             var workbook = await NPOIUtil.Read(excelPath);
             var calculator = new Calculator();
 
             var output = await calculator.Run(workbook, input);
 
-            var dir = Path.Combine(SourceRoot, "data", "validation", situatie.Name);
-            if (!Directory.Exists(dir))
-            {
-                Directory.CreateDirectory(dir);
-            }
-            var outputPath = Path.Combine(dir, $"output-excel-{i}.json");
+            var outputPath = GetOutputPath(input, "output", "-excel");
+
 
             var json = _serializer.SerializeObject(output);
             await File.WriteAllTextAsync(outputPath, json);
 
         }
 
-
-
-        public static IEnumerable<(int, Situatie, Input)> AllScenarios()
+        private string GetOutputPath(Input input, string prefix, string suffix = "")
         {
-            foreach (var situatie in Situatie.All().Where(s => s.Name == "AP"))
+            var situatie = Situatie.ByName(input.Situatie);
+            var dir = Path.Combine(SourceRoot, "data", "validation", situatie.Name);
+            if (!Directory.Exists(dir))
             {
-                for (var i = 0; i < 5; i++)
-                {
-                    var random = new Random(i);
-                    var input = situatie.GenerateInput(random);
-                    input.PlanGegevens.Omschrijving = i.ToString();
-                    yield return (i, situatie, input);
-                }
+                Directory.CreateDirectory(dir);
             }
+            var outputPath = Path.Combine(dir, $"{prefix}-{input.PlanGegevens.Omschrijving}{suffix}.json");
+            return outputPath;
         }
+
+
+
+        public static IEnumerable<Input> AllScenarios() => new InputGenerator().Generate();
 
 
 
